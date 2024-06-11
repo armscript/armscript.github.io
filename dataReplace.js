757501945952cf17a9f7c011ef5e4112e991c1d3
haykantonyan <hayka5367@gmail.com>
2024-06-11 17:31:40 +0400

    added armscript

---
+++ dataReplace.js
@@ -0,0 +1,13 @@
+const data =  [
+    ['tpel', 'alert'],
+    ['sarqel', 'let'],
+    ['sarqelStatic', 'const'],
+    ['ete', 'if'],
+    ['urish', 'else'],
+    ['funk', 'function'],
+    ['sxal', "throw"],
+    ['Tiv', "Number"],
+    ['veradardz', 'return'],
+]
+data.sort((a, b) => b[0].length - a[0].length);
+export default data
